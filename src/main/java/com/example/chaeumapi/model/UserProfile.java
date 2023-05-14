88a0866de5b7c7e281e1c0a53acf13152eff999c
chilyoon <nby5699@gmail.com>
2023-05-15 01:10:12 +0900

    유저프로파일 추가

---
+++ src/main/java/com/example/chaeumapi/model/UserProfile.java
@@ -0,0 +1,50 @@
+package com.example.chaeumapi.model;
+
+import java.sql.Date;
+
+public class UserProfile {
+    private String c_name;
+    private String c_id;
+    private String c_pw;
+    private Date c_date;
+
+    public UserProfile(String c_name, String c_id, String c_pw, Date c_date) {
+        super();
+        this.c_name = c_name;
+        this.c_id = c_id;
+        this.c_pw = c_pw;
+        this.c_date = c_date;
+    }
+
+    public String getC_name() {
+        return c_name;
+    }
+
+    public void setC_name(String c_name) {
+        this.c_name = c_name;
+    }
+
+    public String getC_id() {
+        return c_id;
+    }
+
+    public void setC_id(String c_id) {
+        this.c_id = c_id;
+    }
+
+    public String getC_pw() {
+        return c_pw;
+    }
+
+    public void setC_pw(String c_pw) {
+        this.c_pw = c_pw;
+    }
+
+    public Date getC_date() {
+        return c_date;
+    }
+
+    public void setC_date(Date c_date) {
+        this.c_date = c_date;
+    }
+}
