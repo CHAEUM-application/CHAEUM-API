c8e4a8d5f7ddf8f206f29835a4f988309862e9e7
chilyoon <nby5699@gmail.com>
2023-05-15 00:30:12 +0900

    API 최초 파일 생성

---
+++ src/main/java/com/example/chaeumapi/ChaeumApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.chaeumapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaeumApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChaeumApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chaeumapi/ChaeumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chaeumapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaeumApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
