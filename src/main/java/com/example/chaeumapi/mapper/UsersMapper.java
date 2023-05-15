6e0b817b2a8e36487e78c4718703fc90f44ab5f5
chilyoon <nby5699@gmail.com>
2023-05-15 17:04:29 +0900

    SQL Mapper 생성

---
+++ src/main/java/com/example/chaeumapi/mapper/UsersMapper.java
@@ -0,0 +1,28 @@
+package com.example.chaeumapi.mapper;
+
+import com.example.chaeumapi.model.UsersProfile;
+import org.apache.ibatis.annotations.*;
+
+import java.sql.Date;
+import java.util.List;
+
+
+@Mapper
+public interface UsersMapper {
+
+    @Select("SELECT * FROM Users WHERE c_id=#{c_id}")
+    UsersProfile getUsers(@Param("c_id") String c_id);
+
+    @Select("SELECT * FROM Users")
+    List<UsersProfile> getUsersList();
+
+    @Insert("INSERT INTO Users VALUES(#{c_name}, #{c_id}, #{c_pw}, #{c_date})")
+    int insertUsers(@Param("c_name") String c_name, @Param("c_id") String c_id, @Param("c_pw") String c_pw, @Param("c_date") Date c_date);
+
+    @Update("UPDATE Users SET c_name=#{c_name}, c_pw=#{c_pw} WHERE c_id=#{c_id}")
+    int updateUsers(String c_name, String c_id, String c_pw, Date c_date);
+
+    @Delete("DELETE FROM Users WHERE c_id=#{c_id}")
+    int deleteUsers(@Param("c_id") String c_id);
+
+}
